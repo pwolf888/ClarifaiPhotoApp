e23a7fb46193807a02da4d09b8365fb45a55cf72
Erin Abrams <erinjabrams@gmail.com>
2017-11-26 14:31:40 +1100

    - Added in file to handle default colours for app

---
+++ clarifaiApp/Supporting Files/SafeColors.swift
@@ -0,0 +1,16 @@
+//
+//  SafeColors.swift
+//  clarifaiApp
+//
+//  Created by Erin Abrams on 26/11/17.
+//  Copyright © 2017 partywolfAPPS. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var safeBlue: UIColor {
+        return UIColor(red:0.24, green:0.44, blue:255, alpha:1.0)
+    }
+}
