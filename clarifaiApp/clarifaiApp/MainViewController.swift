a99cbd5718696aa34166739ca54b0cc7015c53e2
Erin Abrams <erinjabrams@gmail.com>
2017-12-17 22:51:12 +1100

    - added in struct to handle device size to later fine tune the constraints across multiple devices.

--- clarifaiApp/clarifaiApp/MainViewController.swift
+++ clarifaiApp/clarifaiApp/MainViewController.swift
@@ -38,7 +38,28 @@ class MainViewController: UIViewController,
     var loaded = false
     var tagOne = "no poem"
     
+    //Check to see which device the app is running on, in order to apply appropriate contraints.
+    struct ScreenSize
+    {
+        static let SCREEN_WIDTH         = UIScreen.main.bounds.size.width
+        static let SCREEN_HEIGHT        = UIScreen.main.bounds.size.height
+        static let SCREEN_MAX_LENGTH    = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+        static let SCREEN_MIN_LENGTH    = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    }
     
+    struct DeviceType
+    {
+        static let IS_IPHONE_4_OR_LESS  = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
+        static let IS_IPHONE_5          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
+        static let IS_IPHONE_6          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
+        static let IS_IPHONE_6P         = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
+        static let IS_IPHONE_7          = IS_IPHONE_6
+        static let IS_IPHONE_7P         = IS_IPHONE_6P
+        static let IS_IPHONE_8          = IS_IPHONE_6
+        static let IS_IPHONE_8P         = IS_IPHONE_6P
+        static let IS_IPHONE_X          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 812.0
+        static let iPad                 = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
+    }
     
     // Load Clarifai API
     override func viewDidLoad() {
@@ -51,6 +72,8 @@ class MainViewController: UIViewController,
         app = ClarifaiApp(apiKey: "ab5e1c0750f14e5685e24b243de99d27")
         
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
